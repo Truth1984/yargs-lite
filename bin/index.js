02975122863d649408ec3ae1d0096d7352ebbd5a
truth1984 <zhengyao365@163.com>
2021-05-12 17:25:15 +0800

    add more test case

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const u = require("awadau");
 const yargslite = require("../index");
 
 let yargs = new yargslite();
@@ -8,5 +9,11 @@ yargs.addHelpDocEntry("--help", "general help page\ndisplay:\tdisplay storage");
 
 yargs.addCommand("display", (storage) => console.log(storage));
 yargs.addHelpDocEntry("display", "display the storage from yargs");
+
+yargs.addCommand("add", (storage) => {
+  let num = 0;
+  storage._.map((i) => (num += u.float(i)));
+  console.log(num);
+});
 
 yargs.run();
